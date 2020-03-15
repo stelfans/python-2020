789f66eb34667c1043559554cfbd4f0eb1903a11
Pedro Dias <pedroffdias@gmail.com>
2020-03-15 17:28:38 +0000

    exemplo FizzBuzz

--- hello.py
+++ hello.py
@@ -5,16 +5,27 @@
 import os
 os.system('clear')
 
-first_name = "Pedro"
-age = 31
-fav_pizza ={
-	"Pedro": "Queijo",
-	"Nuno": "Cogumelos",
-	"Neuza": "Bacon"
-}
-name = True
 
-print(name)
+num = 1
+fizzcount = 0
+buzzcount = 0
+fizzbuzzcount = 0
 
+while (num <= 1000000):
+	if (num % 3 == 0) and (num % 5 == 0):
+		print(str(num) + ": Fizzbuzz!")
+		fizzbuzzcount += 1
+	elif (num % 3 == 0):
+		print(str(num) + ": Fizz!")
+		fizzcount += 1
+	elif (num % 5 == 0):
+		print(str(num) + ": Buzz!")
+		buzzcount += 1
+	else:
+		print(str(num))
+	
+	num +=1
 
-
+print("----")
+print("Fizz\t\tBuzz\t\tFizzBuzz")
+print(str("{:,}".format(fizzcount)) + "\t\t" + str(buzzcount) + "\t\t" + str(fizzbuzzcount))
